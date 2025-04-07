c037656d78754673e214c79a61c4de062758b7bc
ssannejohansson <sannej0hansson@hotmail.com>
2025-04-07 09:51:51 +0200

    Starting code done
    Starting code done

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 // Assignment 6
 
+while (running) {
+    const choice = prompt(`Book tracker
+        1. Add Book
+        2. List Books
+        3. Mark Book as Read 
+        4. Exit 
+        Enter your choice`); 
+
+switch (choice) {
+    case "1": 
+        addBook();
+        break; 
+    case "2":
+        listBooks(); 
+        break;
+    case "3":
+        const title = prompt("Enter the title of the book to mark as read:");
+        markAsRead(title);
+        break;
+    case "4":
+        running = false; 
+        alert("Goodbye!");
+        break;
+    default: 
+        alert("Invalid choice"); 
+
+    }
+}
