20ed72b4d561ad7c2520390871106cafb81f1666
ssannejohansson <sannej0hansson@hotmail.com>
2025-04-07 15:24:17 +0200

    addShow, listShow done
    addShow and listShow done and working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Assignment 6
-const running = true; 
+let running = true; 
 
-const shows = [
+const show = [
     {
         title: "Sons of Anarchy",
         creator: "Kurt Sutter", 
@@ -31,51 +31,51 @@ const shows = [
         creator: "Vince Gilligan", 
         isWatched: false,
     }
-]
+];
 
 
 function addShow () {
-
+    let addTitle = prompt("Please write the title of the show you want to add"); 
+    let addCreator = prompt("Please write the name of the creator of the show"); 
+    show.push({title: addTitle, creator: addCreator}); 
+    alert(`You have now added ${addTitle} by ${addCreator} to your list`);
 }
 
 function listShows () {
-
+return console.log(show); 
 }
 
-function markAsWatched (title) {
+function markAsWatched () {
 
 }
-
-
-
 
 
 while (running) {
-    /*const choice = prompt(`Tv-show tracker
+   const choice = prompt(`Tv-show tracker
         1. Add Show
         2. List Shows
         3. Mark Show as Watched 
         4. Exit 
-        Enter your choice`); */
+        Enter your choice`); 
 
 switch (choice) {
     case "1": 
-        addShow();
+        (addShow());
         break; 
     case "2":
-        listShows(); 
+        listShows();
+        running = false; 
         break;
     case "3":
-        const title = prompt("Enter the title of the show to mark as read:");
+        const title = prompt("Enter the title of the show to mark as watched:");
         markAsWatched(title);
         break;
     case "4":
-        running = false; 
         alert("Goodbye!");
+        running = false; 
         break;
     default: 
         alert("Invalid choice"); 
-
     }
 }
 
